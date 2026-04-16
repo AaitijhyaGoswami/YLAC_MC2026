4c7cb65937b21b59fb42a8f9403090179054a838
Abel <159191851+AaitijhyaGoswami@users.noreply.github.com>
2026-04-17 01:06:49 +0530

    Update viewer.py

--- cad_viewer/viewer.py
+++ cad_viewer/viewer.py
@@ -8,7 +8,18 @@ def app():
 
 Beyond its visual impact, the prototype specifies the granular redistribution of space required to achieve modal equilibrium, featuring a **continuous 3.0m "Clear Path"** and an integrated **1.5m Utility Zone**. This design ensures that the informal economy and utility requirements no longer sabotage pedestrian throughput, but instead coexist within a structured ecosystem. For advocacy and policy synthesis, this module provides the technical baseline needed for DULT or BBMP project approval, anchoring municipal investment in a data-driven justification that prioritizes recovered mobility and high Benefit-Cost Ratios over reactive maintenance.
     """)
+    st.header("What The Street Actually Looks Like")
     
+    colx, coly, colz = st.columns(3)
+
+    with colx:
+        st.image("bazaar1.jpg", use_container_width=True)
+
+    with coly:
+        st.image("bazaar2.jpg", use_container_width=True)
+
+    with coly:
+        st.image("bazaar3.jpg", use_container_width=True)
 
     # --- 1. SPATIAL VISUALIZATION ---
     col_current, col_fixed = st.columns(2)
