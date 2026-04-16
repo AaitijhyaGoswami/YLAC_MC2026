040b4b630e7323c02124ffc32bdf6a1a4ebacb6f
Abel <159191851+AaitijhyaGoswami@users.noreply.github.com>
2026-04-17 01:19:00 +0530

    Fix image paths for CAD viewer

--- cad_viewer/viewer.py
+++ cad_viewer/viewer.py
@@ -13,13 +13,13 @@ def app():
     colx, coly, colz = st.columns(3)
 
     with colx:
-        st.image("cad_viewer\bazaar1.jpg", use_container_width=True)
+        st.image("bazaar1.jpg", use_container_width=True)
 
     with coly:
-        st.image("cad_viewer\bazaar2.jpg", use_container_width=True)
+        st.image("bazaar2.jpg", use_container_width=True)
 
     with colz:
-        st.image("cad_viewer\bazaar3.jpg", use_container_width=True)
+        st.image("bazaar3.jpg", use_container_width=True)
 
     # --- 1. SPATIAL VISUALIZATION ---
     col_current, col_fixed = st.columns(2)
